c2870849a170247c74cd5b01731b81512f5a4cf0
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-06-21 14:55:03 +0300

    Initial commit

---
+++ CSharpTraining/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a string to reverse!");
+
+var input = Console.ReadLine();
+
+Console.WriteLine(input.ReverseString().ToUpper().ReverseString());
+
+
+public static class StringFunctions
+{
+    public static string ReverseString(this string? input)
+    {
+        var chars = input?.ToCharArray();
+        if (chars == null) return input ?? string.Empty;
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+    
+    // generate a method change a string to uppercase
+
+    public static string ToUpper(this string? input)
+    {
+        return input?.ToUpper() ?? string.Empty;
+    }
+}
